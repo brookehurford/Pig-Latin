5ee9dd2a9f867e786daa7983363e5502bf0a149e
Brooke Hurford and Sara Wagner <brookebhurford@gmail.com>
2016-01-20 16:16:33 -0800

    Refactored vowells variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,61 @@
-var pigLatin = function(word) {
-  var splitWord = word.split("");
-  var vowelArray = ["a", "e", "i", "o", "u"]
+var vowelArray = ["a", "e", "i", "o", "u"];
+var vowells = function(letter) {
+var result;
 
   for (var i = 0; i < vowelArray.length; i++) {
-    if (splitWord[0] === vowelArray[i]) {
-      return (word + "ay");
+    if (letter === vowelArray[i]) {
+      result = true;
     }
+    // else if (letter !== vowelArray[i]) {
+    //   result = 0;
+    // }
   }
-  for (var i = 0; i < vowelArray.length; i++) {
-    if (splitWord[0] !== vowelArray[i]) {
-      if (splitWord[1] !== vowelArray[i]) {
-        return (word.slice(2) + word.slice(0,2) + "ay");
-      }
+  if (result === undefined) {
+    return false;
+  }
+  return result;
+}
+
+var consanants2 = function(word) {
+  var splitWord = word.split("");
+  var result = 0;
+  if ((vowells(splitWord[0]) === false) && (vowells(splitWord[1]) === false)) {
+    var result = true;
+    return result;
+  }
+}
+
+var consanants = function(word) {
+  var splitWord = word.split("");
+  if (vowells(splitWord[0]) === false) {
+    return true;
+  }
+}
+
+var pigLatin = function(word) {
+  var splitWord = word.split("");
+  console.log((splitWord[0]));
+  if (vowells(splitWord[0]) === true) {
+    return (word + "ay");
+  }
+  else if (consanants2(word) === true) {
+    return (word.slice(2) + word.slice(0,2) + "ay");
+  }
+  else if (consanants(word) === true) {
     return (word.slice(1) + word.slice(0,1) + "ay");
-    }
   }
 }
 
 
-
-
-//   if (array[0] === "a" || array[0] === "e" || array[0] === "i" || array[0] === "o" || array[0] === "u") {
-//   return (word + "ay");
-//   }
-//   else if (array[0] !== "a" && array[1] !== "a" || array[0] !== "e" && array[1] !== "e" || array[0] !== "i" && array[1] !== "i" || array[0] !== "o" && array[1] !== "o") {
-//   return (word.slice(2) + word.slice(0,2) + "ay");
-//   }
-//   else if (array[0] !== "a" || array[0] !== "e" || array[0] !== "i" || array[0] !== "o" || array[0] !== "u") {
-//   return (word.slice(1) + word.slice(0,1) + "ay");
+//
+//   for (var i = 0; i <= vowelArray.length; i += 1) {
+//     console.log(vowelArray.length)
+//     if (splitWord[0] !== vowelArray[i]) {
+//       if (splitWord[1] !== vowelArray[i]) {
+//         console.log("hello", i)
+//         return (word.slice(2) + word.slice(0,2) + "ay");
+//       }
+//     return (word.slice(1) + word.slice(0,1) + "ay");
+//     }
 //   }
 // }
--- specs/specs.js
+++ specs/specs.js
@@ -2,7 +2,7 @@ describe ('pigLatin', function () {
   it("Words that start with a vowel add 'ay' to the end", function() {
     expect(pigLatin("owl")).to.equal("owlay")
   });
-
+  
   it("Words that start with consonant, move first letter to the end at adds 'ay'", function() {
     expect(pigLatin("cat")).to.equal("atcay")
   });
@@ -10,37 +10,12 @@ describe ('pigLatin', function () {
   it("Words that start with two consonants, move both letters to end and add 'ay'", function() {
     expect(pigLatin("dragon")).to.equal("agondray");
   });
+
+  it("Words that start with 'q' and 'u', move both letters to end and add 'ay'", function() {
+    expect(pigLatin("quite")).to.equal("itequay");
+  });
+
+  it("Words that start with 'y', move first letter to end and add 'ay'", function() {
+    expect(pigLatin("yellow")).to.equal("ellowyay");
+  });
 });
-
-
-
-
-
-
-
-// describe('leapYear', function() {
-//   it("is false for a year that is not divisible by 4, 100, or 400", function() {
-//     expect(leapYear(1993)).to.equal(false);
-//   });
-//
-//   it("is true for years divisible by 4", function() {
-//     expect(leapYear(2004)).to.equal(true);
-//   });
-//
-//   it("is false for years divisible by 100", function() {
-//     expect(leapYear(1900)).to.equal(false);
-//   });
-//
-//   it("is true for years divisible by 400", function () {
-//     expect(leapYear(2000)).to.equal(true);
-//   });
-//
-//   it("is false for words", function () {
-//     expect(leapYear("september")).to.equal(false);
-//   })
-//
-//   it("is false for not a number", function() {
-//     expect(leapYear(NaN)).to.equal(false);
-//   })
-//
-// });
